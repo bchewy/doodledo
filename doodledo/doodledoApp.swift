740e1763c2ac1edb8134f9d3b93d66badad30ec9
Brian Chew <brian@bchewy.com>
2026-01-15 16:04:28 +0800

    Initial Commit

---
+++ doodledo/doodledoApp.swift
@@ -0,0 +1,17 @@
+//
+//  doodledoApp.swift
+//  doodledo
+//
+//  Created by brianchew on 15/1/26.
+//
+
+import SwiftUI
+
+@main
+struct doodledoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
